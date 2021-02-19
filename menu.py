77be7c655391a9a30c6417f5f7b0f1b23cd4a229
Mikhail Yudin <fagci.nsk@gmail.com>
2021-02-20 02:11:40 +0700

    sessions

--- menu.py
+++ menu.py
@@ -20,14 +20,19 @@ CFG = os.path.join(LOCAL_DIR, 'session.yml')
 MENU_CFG = os.path.join(DIR, 'cfg', 'menu.yml')
 
 
-def write_config(cfg):
-    with open(os.path.join(CFG, 'w')) as f:
-        yaml.dump(f, cfg)
+def write_config(file, data={}):
+    with open(file, 'w') as f:
+        if data:
+            yaml.dump(data, f)
+        return data
 
 
-def read_config():
-    with open(os.path.join(CFG)) as f:
-        return yaml.load(f, Loader=yaml.Loader)
+def read_config(file):
+    if os.path.exists(file):
+        with open(file) as f:
+            return yaml.load(f, Loader=yaml.Loader) or {}
+    else:
+        return write_config(file)
 
 
 def get_menu():
@@ -40,6 +45,9 @@ def main():
     print('[!!! WARNING !!!]')
     print('This is prototype at this moment. Use tools directly.')
     print()
+
+    cfg = read_config(CFG)
+
     session_name = ''
 
     if not os.path.exists(SESSIONS_DIR):
@@ -49,18 +57,31 @@ def main():
     session_files = sorted(list(os.walk(SESSIONS_DIR))[0][2])
     session_names = [f.rpartition('.')[0] for f in session_files]
 
-    if session_names:
-        session_name = prompt({
-            'type': 'list',
-            'name': 'session_name',
-            'message': 'Choose session',
-            'choices': session_names,
-        }).get('session_name')
-    else:
-        session_name = prompt(menu.get('new_session')).get('session_name')
-        session_name = str_to_filename(session_name)
+    last_session = cfg.get('last_session')
+
+    if last_session and last_session in session_names:
+        if prompt(menu.get('restore_last_session')).get('restore'):
+            session_name = last_session
+        else:
+            if session_names:
+                session_name = prompt({
+                    'type': 'list',
+                    'name': 'session_name',
+                    'message': 'Choose session',
+                    'choices': session_names,
+                }).get('session_name')
+            else:
+                session_name = prompt(
+                    menu.get('new_session')).get('session_name')
+                session_name = str_to_filename(session_name)
 
     session_name = session_name or 'default'
+    session_file = os.path.join(SESSIONS_DIR, f'{session_name}.yml')
+
+    session = read_config(session_file)
+
+    cfg['last_session'] = session_name
+    write_config(CFG, cfg)
 
     print(session_name)
 
