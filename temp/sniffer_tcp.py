c5120ac5b3589a963cd23e1f32314c5ca617243d
Mikhail Yudin <fagci.nsk@gmail.com>
2021-03-04 23:39:48 +0700

    cleanup

--- temp/sniffer_tcp.py
+++ temp/sniffer_tcp.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
+"""Simple tcp sniffer"""
 
-""" 
-Simple tcp sniffer
-"""
-
-import socket, struct
+import socket
+import struct
 
 s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
 
+
 def b2mac(bytes_addr):
     return ':'.join(map('{:02x}'.format, bytes_addr))
+
 
 def main():
     while True:
@@ -27,13 +27,14 @@ def main():
         src_mac = b2mac(eth_struct[1])
         dst_mac = b2mac(eth_struct[0])
 
-        if proto_type == 8: # IP
+        if proto_type == 8:  # IP
             ip_struct = struct.unpack('!BBHHHBBH4s4s', ip_header)
             src_ip = ip_struct[8]
             dst_ip = ip_struct[9]
-            if ip_struct[6] == 6: # TCP
+            if ip_struct[6] == 6:  # TCP
                 src_port, dst_port, _ = struct.unpack('!HH16s', tcp_header)
-                print('\n{} > {}\n{}:{} > {}:{}'.format(src_mac, dst_mac, socket.inet_ntoa(src_ip), src_port, socket.inet_ntoa(dst_ip), dst_port))
+                print('\n{} > {}\n{}:{} > {}:{}'.format(src_mac, dst_mac, socket.inet_ntoa(
+                    src_ip), src_port, socket.inet_ntoa(dst_ip), dst_port))
                 if b'HTTP' in data:
                     ln = data.split(b'\r\n')
                     for l in ln:
@@ -41,6 +42,7 @@ def main():
                             print(str(l))
                         except Exception as e:
                             print(e)
+
 
 if __name__ == "__main__":
     try:
