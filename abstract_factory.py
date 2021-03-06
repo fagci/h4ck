47fdaab3901f80ee1d0e1668732004b582b86fe2
Mikhail Yudin <fagci.nsk@gmail.com>
2021-03-07 00:36:35 +0700

    pipeline attempt

--- abstract_factory.py
+++ abstract_factory.py
@@ -6,55 +6,31 @@ from time import sleep
 from lib.scan import generate_ips
 
 
-class Work(Thread):
-    q_in: Queue
-    q_out: Queue
-    workers: list[Thread]
+class Node:
+    source = []
+    sink = None
 
-    def __init__(self, workers_count=16, qo_size=16):
-        super().__init__()
-        self.q_out = Queue(qo_size)
-        self._workers_count = workers_count
+    @staticmethod
+    def connect(source, sink):
+        sink.sink = source.source
+        return sink
 
-        self.workers = []
-        self.setDaemon(True)
+    def __or__(self, sink: 'Node'):
+        return self.connect(self, sink)
 
-    def work(self):
-        while True:
-            if self.q_out.not_full:
-                self.q_out.put(self.q_in.get())
-
-    def run(self):
-        for _ in range(self._workers_count):
-            w = Thread(target=self.work, daemon=True)
-            w.start()
-
-    def __or__(self, b: 'Work'):
-        b.q_in = self.q_out
-        return b
-
-    def __ror__(self, b: 'Work'):
-        b.q_in = self.q_out
-        return b
+    def __ror__(self, source: 'Node'):
+        return self.connect(source, self)
 
     def __iter__(self):
-        while any(map(lambda x: x.is_alive(), self.workers)):
-            sleep(0.25)
-            yield self.q_out.get(timeout=10)
+        yield from self.source or self.sink
 
 
-class HostGen(Work):
-    def run(self):
-        self.ips = generate_ips(100)
-        super().run()
-
-    def work(self):
-        while True:
-            if self.q_out.not_full:
-                self.q_out.put(next(self.ips))
+class HostGen(Node):
+    def __init__(self):
+        self.source = generate_ips(100)
 
 
-class Fuzzer(Work):
+class Fuzzer(Node):
     pass
 
 
@@ -62,19 +38,10 @@ def main():
     hosts = HostGen()
     fuzzer = Fuzzer()
 
-    print('Connecting')
-
     results = hosts | fuzzer
-
-    print('Connected')
-
-    fuzzer.start()
-    hosts.start()
 
     for result in results:
         print(result)
-
-    results.join()
 
 
 if __name__ == "__main__":
