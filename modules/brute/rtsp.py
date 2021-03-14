88782ee6c24e600d5c3ac597c8fb6cb22c3cf032
Mikhail Yudin <fagci.nsk@gmail.com>
2021-03-15 02:56:09 +0700

    refactor

---
+++ modules/brute/rtsp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import logging
+from fire import Fire
+
+from lib.fuzz import Brute, Fuzz, ListFile
+from lib.net import RTSPConnection, logger
+from lib.scan import process_threaded
+
+
+def process_host(interface, host):
+    with RTSPConnection(host, 554, interface) as connection:
+        for fuzz_result in Fuzz(connection):
+            existing_path = fuzz_result.path
+
+            if fuzz_result.ok:
+                return connection.url(existing_path)
+
+            if fuzz_result.auth_needed:
+                for cred in Brute(connection, existing_path):
+                    return connection.url(existing_path, cred)
+                break
+
+
+def main(hosts_file: str, w: int = None, i: str = '', sp: bool = True, d: bool = False):
+    from functools import partial
+
+    if d:
+        logger.setLevel(logging.DEBUG)
+        h = logging.StreamHandler()
+        logger.addHandler(h)
+
+    hosts = ListFile(hosts_file)
+
+    ph = partial(process_host, i)
+
+    urls = process_threaded(ph, hosts, callback=lambda x: bool(x), workers=w)
+
+    for url in urls:
+        print(url)
+
+
+if __name__ == "__main__":
+    Fire(main)
