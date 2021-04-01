82fdf7a6fa1030cca966df78cc943c03e26f5c0d
Mikhail Yudin <fagci.nsk@gmail.com>
2021-04-02 02:15:50 +0700

    customize output support

--- results.py
+++ results.py
@@ -7,14 +7,15 @@ from lib.models import Port, Target, db_session, sql_debug
 
 
 @db_session
-def main(query='', port=None):
-    if not query:
+def main(query='', port=None, tags=None, sdt=False, sp=True, sc=True, sb=False, sd=False, st=False):
+    """Display stats or search in db"""
+    if not any((query, port, tags)):
         print('Stats by port')
         select((p.num, p.tags, count(p)) for p in Port).show()
         return
 
     res = select(
-        (p.created_at, t.ip, p.num, p.comment)
+        (p.created_at, t.ip, p.num, p.comment, p.banner, p.data, p.tags)
         for t in Target for p in t.ports
         if (
             query in p.comment
@@ -22,12 +23,29 @@ def main(query='', port=None):
             query in p.banner
             or
             query in t.ip
-        ) and (port == None or port == p.num)
+        )
+        and
+        (port == None or port == p.num)
+        and
+        (tags == None or tags in p.tags)
     )
 
-    for d, ip, p, c in res:
-        print('{} {} {} {}'.format(
-            d.strftime('%m-%d %H:%M'), ip, p, c))
+    for d, ip, p, c, b, data, t in res:
+        parts = []
+        if sdt:
+            parts.append(d.strftime('%m-%d %H:%M'))
+        parts.append(ip)
+        if sp:
+            parts.append(p)
+        if sc:
+            parts.append(c)
+        if sb:
+            parts.append(b)
+        if sd:
+            parts.append(data)
+        if st:
+            parts.append(','.join(t))
+        print(*parts)
 
 
 if __name__ == "__main__":
