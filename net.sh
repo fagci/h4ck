31ccff2e9d6afcec53cf11940a530558eccc1b08
Mikhail Yudin <fagci.nsk@gmail.com>
2021-04-05 03:22:48 +0700

    network functions

---
+++ net.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+tcp_conn() {
+    exec 3<>/dev/tcp/$1/$2
+}
+
+tcp_close() {
+    exec 3<&-
+    exec 3>&-
+}
+
+tcp_send() {
+    printf "$1" >&3
+}
+
+tcp_recv() {
+    timeout ${1:-5} cat <&3
+}
+
+tcp_check_port() {
+    tcp_conn $1 $2 2> /dev/null
+    [ ! $? -eq 0 ] && return 1
+    tcp_close
+}
+
