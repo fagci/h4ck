d5c44cbaefee386899e15a244e568580aaf0f17f
Mikhail Yudin <fagci.nsk@gmail.com>
2021-04-05 10:01:14 +0700

    fix diag warnings

--- net.sh
+++ net.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 tcp_conn() {
-    exec 3<>/dev/tcp/$1/$2
+    exec 3<>"/dev/tcp/$1/$2"
 }
 
 tcp_close() {
@@ -10,15 +10,14 @@ tcp_close() {
 }
 
 tcp_send() {
-    printf "$1" >&3
+    echo -e "$1" >&3
 }
 
 tcp_recv() {
-    timeout ${1:-5} cat <&3
+    timeout "${1:-5}" cat <&3
 }
 
 tcp_check_port() {
-    tcp_conn $1 $2 2> /dev/null || return $?
+    tcp_conn "$1" "$2" 2> /dev/null || return $?
     tcp_close
 }
-
