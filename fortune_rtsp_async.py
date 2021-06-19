cc266ddb0d3cdeeb45a84134935c86ce9f099563
Mikhail Yudin <fagci.nsk@gmail.com>
2021-06-19 15:17:46 +0700

    rtsp async fortune

---
+++ fortune_rtsp_async.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+from lib.scan import generate_ips
+from lib.net import RTSPConnection, Response
+
+from fire import Fire
+
+async def worker(q_in, q_out, port, timeout=0.75):
+    while True:
+        ip = await q_in.get()
+        c = asyncio.open_connection(ip, port)
+        try:
+            await asyncio.wait_for(c, timeout)
+        except:
+            pass
+        else:
+            q_out.put_nowait(ip)
+            with RTSPConnection(ip, port) as c:
+                r = c.query()
+
+                if r.found and 'PLAY' in r.headers.get('public', ''):
+                    print(ip)
+        finally:
+            q_in.task_done()
+
+async def filler(q_in, tasks_q_filled, limit):
+    for ip in generate_ips(limit):
+        await q_in.put(ip)
+    tasks_q_filled.set()
+
+async def main(port=554, t=0.75, w=256, l=20000):
+    tasks_q = asyncio.Queue(w)
+    out_q = asyncio.Queue()
+    tasks_q_filled = asyncio.Event()
+
+    tasks = [asyncio.create_task(filler(tasks_q, tasks_q_filled, l))]
+
+    for _ in range(w):
+        f = worker(tasks_q, out_q, port, t)
+        task = asyncio.create_task(f)
+        tasks.append(task)
+
+    await tasks_q_filled.wait()
+    await tasks_q.join()
+
+    for task in tasks:
+        task.cancel()
+
+    # while out_q.qsize():
+    #     print(out_q.get_nowait(), flush=True)
+
+if __name__ == '__main__':
+    Fire(main)
